3ef56dbfd22032815560f6425cc9d2b0c3913d05
Andrea Padgett <agpadgett89@gmail.com>
2015-08-17 20:11:41 -0700

    check

---
+++ src/test/RectangleTest.java
@@ -0,0 +1,17 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class RectangleTest{
+
+  @Test
+  public void newRectangle_instatinatesCorrectly(){
+    Rectangle testRectangle = new Rectangle(2, 2);
+    assertEquals(true, testRectangle instanceof Rectangle);
+  }
+
+  @Test
+  public void isSquare_whenNotASquare_false(){
+    Rectangle testRectangle = new Rectangle(2, 4);
+    assertEquals(false, testRectangle.isSquare());
+  }
+}
